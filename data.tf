facccc1841e5f9f85c5e92d1451f0cb87a67491a
kotadurga2027 <kotadurga1997@gmail.com>
2026-03-17 09:07:02 +0530

    commit this

---
+++ data.tf
@@ -0,0 +1,21 @@
+
+data "aws_ami" "roboshop" {
+
+    most_recent = true
+    owners = ["405832638662"]
+
+    filter {
+        name   = "name"
+        values = ["Redhat-9-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
